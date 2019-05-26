d4d079c6864769882bf31d8691da0b5b68602a30
Roberto Gonzalez-Ramos <rgonzalezramos@gmail.com>
2019-05-27 00:15:22 +0200

    add mysql and a /db endpoint to see how spans with db calls look

--- src/main/java/rob/openapmjavaexample/Endpoints.java
+++ src/main/java/rob/openapmjavaexample/Endpoints.java
@@ -3,17 +3,26 @@ package rob.openapmjavaexample;
 import co.elastic.apm.api.ElasticApm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.persistence.EntityManager;
+import java.sql.Timestamp;
 import java.util.Random;
 
 @Controller
 public class Endpoints {
     private Random random = new Random();
     private static final Logger logger = LoggerFactory.getLogger(Endpoints.class);
+    private final EntityManager entityManager;
+
+    @Autowired
+    public Endpoints(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @RequestMapping(value = "/fast", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
@@ -28,6 +37,14 @@ public class Endpoints {
         someSlowWork();
         moreSlowWork();
         return "Hello";
+    }
+
+    @RequestMapping(value = "/db", method = RequestMethod.GET, produces = "text/plain")
+    @ResponseBody
+    public String db() throws InterruptedException {
+        Timestamp ts = (Timestamp) entityManager.createNativeQuery("SELECT NOW()")
+                .getSingleResult();
+        return ts.toString();
     }
 
     @RequestMapping(value = "/crash", method = RequestMethod.GET, produces = "text/plain")
--- src/main/java/rob/openapmjavaexample/OpenapmJavaExampleApplication.java
+++ src/main/java/rob/openapmjavaexample/OpenapmJavaExampleApplication.java
@@ -3,6 +3,7 @@ package rob.openapmjavaexample;
 import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenapmJavaExampleApplication {
