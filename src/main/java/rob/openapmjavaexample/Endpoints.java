ae616d0e82cd6c37fc3fdf118b4b7e12a92b173a
Roberto Gonzalez-Ramos <rgonzalezramos@gmail.com>
2019-05-26 17:18:11 +0200

    add trace_methods

--- src/main/java/rob/openapmjavaexample/Endpoints.java
+++ src/main/java/rob/openapmjavaexample/Endpoints.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Random;
+
 @Controller
 public class Endpoints {
+    private Random random = new Random();
+
     @RequestMapping(value = "/fast", method = RequestMethod.GET, produces = "text/plain")
     public @ResponseBody String fast() {
         return "Hello";
@@ -14,7 +18,16 @@ public class Endpoints {
 
     @RequestMapping(value = "/slow", method = RequestMethod.GET, produces = "text/plain")
     public @ResponseBody String slow() throws InterruptedException {
-        Thread.sleep(1000);
+        someSlowWork();
+        moreSlowWork();
         return "Hello";
+    }
+
+    private void someSlowWork() throws InterruptedException {
+        Thread.sleep(900 + random.nextInt(200));
+    }
+
+    private void moreSlowWork() throws InterruptedException {
+        Thread.sleep(450 + random.nextInt(100));
     }
 }
