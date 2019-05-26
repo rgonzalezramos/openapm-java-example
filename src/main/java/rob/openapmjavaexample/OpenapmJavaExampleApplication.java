ea1761a7bd7736671b265c28405a4d4d4f929865
Roberto Gonzalez-Ramos <rgonzalezramos@gmail.com>
2019-05-26 16:11:58 +0200

    add base spring web project

---
+++ src/main/java/rob/openapmjavaexample/OpenapmJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package rob.openapmjavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapmJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapmJavaExampleApplication.class, args);
+	}
+
+}
